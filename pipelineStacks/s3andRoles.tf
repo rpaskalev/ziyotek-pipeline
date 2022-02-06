f9f339b4071cbe278ed383763ef08eeed82ea5de
rpaskalev <rpaskalev@lomixtech.com>
2022-02-06 15:20:41 -0500

    destroy

--- pipelineStacks/s3andRoles.tf
+++ pipelineStacks/s3andRoles.tf
@@ -67,55 +67,6 @@ resource "aws_iam_policy" "codepipeline_policy" {
 EOF
 }
 
-resource "aws_iam_user" "user" {
-  name = "test-user"
-}
-
-resource "aws_iam_role" "role" {
-  name = "pipe-role"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_group" "group" {
-  name = "test-group"
-}
-
-resource "aws_iam_policy" "policy" {
-  name        = "test-policy"
-  description = "A test policy"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "ec2:Describe*",
-        "ssm:*"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-}
-
 resource "aws_iam_role_policy_attachment" "test-attach" {
   role       = aws_iam_role.codepipeline_role.name
   policy_arn = aws_iam_policy.codepipeline_policy.arn
@@ -123,4 +74,5 @@ resource "aws_iam_role_policy_attachment" "test-attach" {
 
 variable "aritifact_bucket_name" {
   description = "the name you need to give to an artifact bucket"
+  default = "rady-bucket-pipeline-artifacts"
 }
